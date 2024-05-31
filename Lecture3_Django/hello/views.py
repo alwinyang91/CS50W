f8518da7a727cee3830234cbd5075a7fb329ff23
Zewen <zewenyang91@gmail.com>
2024-05-31 11:51:19 +0200

    add functions

--- Lecture3_Django/hello/views.py
+++ Lecture3_Django/hello/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+def index(request):
+    return render(request, 'hello/index.html')
+    # return HttpResponse("Hello, world. You're at the polls index.")
+
+def users(request):
+    return HttpResponse("Hello, User.")
+
+def greet(request, name):
+    return HttpResponse(f"Hello, {name.capitalize()}!")
