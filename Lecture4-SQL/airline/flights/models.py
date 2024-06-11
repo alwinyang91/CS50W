2fb75fd1d18c3f2924b6bebac20e59fc84712f8c
Zewen <zewenyang91@gmail.com>
2024-06-11 16:33:16 +0200

    add flight app

--- Lecture4-SQL/airline/flights/models.py
+++ Lecture4-SQL/airline/flights/models.py
@@ -2,7 +2,40 @@ from django.db import models
 
 # Create your models here.
 
+# class Flight(models.Model):
+#     origin = models.CharField(max_length=64)
+#     destination = models.CharField(max_length=64)
+#     duration = models.IntegerField()
+
+#     def __str__(self):
+#         return f"{self.id}: {self.origin} to {self.destination}"
+
+class Airport(models.Model):
+    code = models.CharField(max_length=3)
+    city = models.CharField(max_length=64)
+
+    def __str__(self):
+        return f"{self.city} ({self.code})"
+
 class Flight(models.Model):
-    origin = models.CharField(max_length=64)
-    destination = models.CharField(max_length=64)
+    # refer the Airport table
+    # the contraints on_delete=models.CASCADE when the flight is deleted also delete the corrsponsonding flight
+    # the argument related_name is a way of accessing the relationship in reverse order
+    # e.g., when I have an Airport has all the flight that the airport is the origin
+    origin = models.ForeignKey(Airport, on_delete=models.CASCADE, related_name="departures")
+    destination = models.ForeignKey(Airport, on_delete=models.CASCADE, related_name="arrivals")
     duration = models.IntegerField()
+
+    def __str__(self):
+        return f"{self.id}: {self.origin} to {self.destination}"
+    
+
+class Passenger(models.Model):
+    first_name = models.CharField(max_length=64)
+    last_name = models.CharField(max_length=64)
+    # blank=True allows passenger to be empty
+    # Flight.passengers can get all the passengers that have this flight
+    flights = models.ManyToManyField(Flight, blank=True, related_name="passengers")
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
--- Lecture4-SQL/airline/flights/views.py
+++ Lecture4-SQL/airline/flights/views.py
@@ -1,3 +1,29 @@
 from django.shortcuts import render
 
+from .models import Flight, Passenger
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+
 # Create your views here.
+def index(request):
+    return render(request, "flights/index.html", {
+        "flights": Flight.objects.all()
+    })
+
+# flight_id is given when visting the path /flights/flight_id
+def flight(request, flight_id):
+    # flight_id is the primary key of the flight
+    flight = Flight.objects.get(pk=flight_id)
+    return render(request, "flights/flight.html", {
+        "flight": flight,
+        "passengers": flight.passengers.all(),
+        "non_passengers": Passenger.objects.exclude(flights=flight).all()
+    })
+
+def book(request, flight_id):
+    if request.method == "POST":
+        flight = Flight.objects.get(pk=flight_id)
+        passenger_id = int(request.POST["passenger"])
+        passenger = Passenger.objects.get(pk=passenger_id)
+        passenger.flights.add(flight)
+        return HttpResponseRedirect(reverse("flight", args=(flight.id)))
